b7e6b0f8a08eb1a1646533008af177230f2b5010
Andrew Skelland <cipher_no1@hotmail.com>
2019-10-03 01:18:29 +0100

    Changed character animations to us animation events within mecanim

--- Assets/_Scripts/Handlers/Scene/Objects/DoorHandler.cs
+++ Assets/_Scripts/Handlers/Scene/Objects/DoorHandler.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Diagnostics.CodeAnalysis;
 using com.ArkAngelApps.TheAvarice.Helpers.InputSystem;
 using com.ArkAngelApps.TheAvarice.Scriptable.Characters;
 using com.ArkAngelApps.TheAvarice.Scriptable.System;
